2e4a1a37f1add5dd9f8ec35caab1a51bf1514106
charles <chevalier.charles@gmail.com>
2019-09-26 15:14:44 +0200

    Mojjo: added more methods to badge for custom needs

--- app/models/merit/badge.rb
+++ app/models/merit/badge.rb
@@ -34,6 +34,93 @@ module Merit
       {sash_id: sashes}
     end
 
+    # Custom mojjo methods
+    def earned_on(user)
+      Merit::BadgesSash.where(sash_id: user.sash_id, badge_id: self.id).first.try(:created_at)
+    end
+
+    def image
+      ActionController::Base.helpers.asset_path("images/badges/#{self.name}.png")
+    end
+
+    def value
+      self.custom_fields[:value]
+    end
+
+    def unit 
+      self.custom_fields[:unit]
+    end
+
+    def category
+      self.custom_fields[:category]
+    end
+
+    def sub_category
+      self.custom_fields[:sub_category]
+    end
+
+    def ressource 
+      self.custom_fields[:ressource]
+    end
+
+    def points
+      self.custom_fields[:points]
+    end
+
+    def title
+      "#{self.id}-#{self.name}#{self.level}"
+    end
+
+    def min_matches
+      self.custom_fields[:min_matches] || 0
+    end
+
+    def progress(user)
+      res = self.custom_fields[:rule].call(user) if self.custom_fields[:rule].class == Proc
+    end
+
+    def grantable?(user)
+      raise "No rule found for badge #{self.title}" unless self.custom_fields[:rule].class == Proc
+      self.value ? self.progress(user) >= value : self.progress(user) == true
+    end
+
+    def locale_name
+      I18n.t "badges.#{self.name}.name", default: self.name
+    end
+
+    def locale_description
+      I18n.t "badges.#{self.name}.desc", default: self.description, value: self.value
+    end
+
+    def level_count
+      badge = BADGES.select {|b| b[:name] == self.name}.first
+      if badge[:levels]
+        badge[:levels].count
+      else
+        1
+      end
+    end
+
+    def json(user = nil, show_progress = false)
+    {
+        id: self.id,
+        name: self.locale_name,
+        key: self.name,
+        title: self.title,
+        level: self.level || 1,
+        level_count: self.level_count,
+        points: self.points,
+        value: self.value,
+        unit: self.unit,
+        image: self.image,
+        category: self.category,
+        sub_category: self.sub_category,
+        description: self.locale_description,
+        progress: user && show_progress && self.level == 1 ? self.progress(user) : nil,
+        earned_on: user ? self.earned_on(user) : nil
+    }
+    end
+
     class << self
       def find_by_name_and_level(name, level)
         badges = Merit::Badge.by_name(name)
@@ -52,6 +139,27 @@ module Merit
         define_method(:"#{meritable_class_name.underscore.pluralize}") do
           sashes = BadgesSash.where(badge_id: id).pluck(:sash_id)
           meritable_class_name.constantize.where(send "_#{Merit.orm}_sash_in", sashes)
+        end
+      end
+
+      # Custom mojjo methods
+      def seek(name, level = nil)
+        self.find_by_name_and_level(name, level)
+      end
+
+      def by_ressource(ressource)
+        self.select { |b| b.ressource == ressource }
+      end
+
+      def category(category, sub_category = nil)
+        self.select do |b|
+          if b.category != category
+            false
+          elsif !sub_category.nil? && b.sub_category != sub_category
+            false
+          else
+            true
+          end
         end
       end
     end
